7b642a5f62c0b284f123c7f1d244a69f2816b64f
Aysu Rahimli 18053 <arahimli18053@ada.edu.az>
2024-11-15 16:22:20 +0400

    popup.js file initial steps

--- popup.js
+++ popup.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+document.getElementById("saveProfile").addEventListener("click", () => {
+    const profileName = document.getElementById("profileName").value;
+    if (profileName) {
+        saveProfile(profileName);
+        document.getElementById("profileName").value = "";
+    }
+});
+
